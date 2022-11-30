dc0870d4bce367daf522e5a5a4fb6954ecd17b81
unknown <jose.sandele@gmail.com>
2022-11-30 08:11:02 +0100

    echart works

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from 'src/app/layouts/header/header.component';
 
+import * as echarts from 'echarts';
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,18 +11,70 @@ import { HeaderComponent } from 'src/app/layouts/header/header.component';
 })
 export class DashboardComponent implements OnInit {
 
+
+
   logoOn: any;
 
   sideBarOpen = true;
 
-  constructor( public img: HeaderComponent ) { }
+  constructor(public img: HeaderComponent) { }
 
   ngOnInit(): void {
-   //this.logoOn === this.img.hideImg();
+    //this.logoOn === this.img.hideImg();
+
+    type EChartsOption = echarts.EChartsOption;
+
+    var chartDom = document.getElementById('main')!;
+    var myChart = echarts.init(chartDom);
+    var option: EChartsOption;
+
+    option = {
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        bottom: '-2%',
+        left: 'center'
+      },
+      series: [
+        {
+          name: 'Dados',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            show: true,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: false,
+              fontSize: '12',
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 1048, name: 'Arroz' },
+            { value: 735, name: 'Milho' },
+            { value: 580, name: 'Feijão' },
+            { value: 484, name: 'Ovos' },
+          ]
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+
+
   }
 
-  sideBarToggler(){
+  sideBarToggler() {
     this.sideBarOpen = !this.sideBarOpen;
   }
+
+
 
 }
