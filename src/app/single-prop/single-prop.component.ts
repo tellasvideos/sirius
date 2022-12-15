f84d587debcecfd5cbe8de6b39b1cb427e6a5862
unknown <jose.sandele@gmail.com>
2022-12-15 15:56:58 +0100

    single-prop works

--- src/app/single-prop/single-prop.component.ts
+++ src/app/single-prop/single-prop.component.ts
@@ -11,12 +11,21 @@ export class SinglePropComponent implements OnInit {
 
   public proponente: any;
 
+  sideBarOpen = true;
+
+  //modalRef: MdbModalRef<AddInqueritoComponent> | null = null;
+
   constructor(private router: Router, private route: ActivatedRoute, private dataService: DataService) {
     this.dataService.proponentPDAC().subscribe(data => {
       this.proponente = data;
       console.log(data)
     })
   }
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
+
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(res => {
@@ -27,5 +36,8 @@ export class SinglePropComponent implements OnInit {
       this.proponente = data;
     })
   }
+
+
+
 
 }
