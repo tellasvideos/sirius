540a4a51e0fe30f4228918a55e491f70c807c79b
unknown <jose.sandele@gmail.com>
2023-01-11 12:23:17 +0100

    inquerito done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,8 @@ export class HomeComponent implements OnInit {
 
   }]
 
+  _errorLogin = false;
+
   pass_: any;
 
   constructor(private auth: DataService, private route: Router) { }
@@ -24,37 +26,86 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
-  login(email:any, pass:any){
-
-   
+  login(email: any, pass: any) {
     this.user[0].username == email;
     this.user[0].password == pass;
-    //const LogInData = { username: email, password: pass }
     console.log(this.user)
-
     localStorage.removeItem('user');
-    this.auth.userLogin(this.user).subscribe(data =>{
+    this.auth.userLogin(this.user).subscribe(data => {
       this.user = data
-      // 
       console.log('Depois do login', data);
-      if(this.user.token){
-        localStorage.setItem("userToken", this.user.token);
-        this.route.navigate(['/dashboard'])
-      }else{
+
+      try {
+
+        if (this.user.token) {
+          this._errorLogin = false;
+          localStorage.setItem("userToken", this.user.token);
+          this.route.navigate(['/dashboard'])
+        } else {
+        }
+
+      } catch (error) {
+        this._errorLogin = true;
         this.errorLogin()
+
       }
 
-    })
 
+    })
   }
 
-  errorLogin(){
+  errorLogin() {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
       text: 'Usuário não autorizado!',
     })
   }
+
+  /* 
+  loginParceiro(mail: any) {
+
+    this.usern[0].email == mail;
+    //console.log(mail)
+    localStorage.removeItem('user');
+    this.sub.getUserAfricaplay(mail, this.token).subscribe(data => {
+      this.user = data;
+      //console.log(data)
+      //console.log(this.sub.getUserAfricaplay)
+      try {
+        if (this.user[0].email) {
+          this.errorLogin = false;
+          this.auth.setSession(this.user);
+          localStorage.setItem('profile', JSON.stringify(this.user[0]));
+          localStorage.setItem('user', JSON.stringify(this.user[0]));
+          this.rt.navigate(['/destaques']);
+          //this.sub.getUserAfricaplay(this.user[0].ID);
+          this.sub.isActive();
+        } else {
+
+        }
+      } catch (error) {
+        this.errorLogin = true;
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Conta Inválida!',
+          width: '500',
+          confirmButtonColor: "#fbb03b",
+          confirmButtonText: `Tentar novamente`,
+        });
+        this.user = [{
+          ID: '',
+          created: '',
+          email: '',
+          fimSubscricao: '',
+          pass: '',
+          status: '',
+          sub: ''
+        }];
+      }
+    })
+  }
+   */
 
 }
