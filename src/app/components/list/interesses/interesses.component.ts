2c62d0726ae01fce507d83a4517010e8b61b49c0
unknown <jose.sandele@gmail.com>
2023-06-21 01:38:17 +0100

    ajustes no backofice

--- src/app/components/list/interesses/interesses.component.ts
+++ src/app/components/list/interesses/interesses.component.ts
@@ -6,7 +6,8 @@ import Swal from 'sweetalert2';
 import { SingleInterestComponent } from 'src/app/components/single-view/single-interest/single-interest.component';
 import { AddInteressesComponent } from '../../inserts/add-interesses/add-interesses.component';
 import { AddAndManifestComponent } from '../../inserts/add-and-manifest/add-and-manifest.component';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-interesses',
@@ -14,6 +15,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./interesses.component.scss']
 })
 export class InteressesComponent implements OnInit {
+
+  historico_de_producao_ = [
+    'Café',
+    'Milho',
+    'Feijão',
+    'Soja',
+    'Batata Rena',
+    'Batata Doce']
+  user_logged: any;
 
   today: Date = new Date();
   //minDate: Date = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() -31);
@@ -36,11 +46,12 @@ export class InteressesComponent implements OnInit {
     private modalService: MdbModalService,
     private dataService: DataService,
     private fb: FormBuilder,
+    private http: HttpClient
   ) {
 
     this.angForm = this.fb.group({
 
-      consultor_pn_name: ['', Validators.required],
+      consultor_pn: ['', Validators.required],
       inicio_elaboracao_pn: ['', Validators.required],
       fim_elaboracao_pn: [''],
       fim_verificacao: [''],
@@ -52,20 +63,30 @@ export class InteressesComponent implements OnInit {
       historico_producao_2_anos: ['', Validators.required],
       area_cultura_2_anos: ['', Validators.required],
       producao_cultura_2_anos: ['', Validators.required],
-      estudo_de_viabilidade: ['', Validators.required],
+
+      estudo_de_viabilidade: new FormControl(null),
+      termo_compromisso_assinado: new FormControl(null),
+      projeto_riv_completo: new FormControl(null),
+      ftas: new FormControl(null),
+      lista_de_trabalhadores: new FormControl(null),
+      documentos_administrativos: new FormControl(null),
+      ficheiro_riv: new FormControl(null),
+      outros_documentos: new FormControl(null),
+
+      /*estudo_de_viabilidade: ['', Validators.required],
       termo_compromisso_assinado: ['', Validators.required],
       projeto_riv_completo: ['', Validators.required],
       ftas: ['', Validators.required],
       lista_de_trabalhadores: ['', Validators.required],
       documentos_administrativos: ['', Validators.required],
       ficheiro_riv: [''],
-      outros_documentos: [''],
+      outros_documentos: [''],*/
       data_pn_entregue_ao_pdac: [''],
-      pn_pendente: [''],
+      pn_pendente: [false],
       justificacao_pn_pendente: ['', Validators.required],
-      proponente_desistiu: [''],
+      proponente_desistiu: [false],
       created_at: [''],
-      consultor_pn: ['']
+      //consultor_pn: ['']
     })
   }
 
@@ -73,7 +94,115 @@ export class InteressesComponent implements OnInit {
     this.getProvincias()
     this.list_interest()
     this.getInqueritos()
+    this.checkDates()
+    this.getUserBackOFF()
+
+    // Pegar dados do user logado
+    this.dataService.getUserData().subscribe((data: any) => {
+      this.user_logged = data.find((user: any) => user.email === localStorage.getItem('user'));
+      console.log('User logado', this.user_logged)
+    });
   }
+
+  enviarFormulario() {
+    if (this.angForm.valid) {
+      const formData = new FormData();
+
+      formData.append('consultor_pn', this.angForm.get('consultor_pn')?.value);
+      formData.append('inicio_elaboracao_pn',(this.angForm.get('inicio_elaboracao_pn')?.value));
+      formData.append('fim_elaboracao_pn', (this.angForm.get('fim_elaboracao_pn')?.value));
+      formData.append('fim_verificacao', (this.angForm.get('fim_verificacao')?.value));
+      formData.append('area_total_fazenda', this.angForm.get('area_total_fazenda')?.value);
+      formData.append('area_cultivo_pn', this.angForm.get('area_cultivo_pn')?.value);
+      formData.append('recursos_proprios', this.angForm.get('recursos_proprios')?.value);
+      formData.append('financiamento', this.angForm.get('financiamento')?.value);
+      formData.append('financiamento_bancario', this.angForm.get('financiamento_bancario')?.value);
+      formData.append('historico_producao_2_anos', this.angForm.get('historico_producao_2_anos')?.value);
+      formData.append('area_cultura_2_anos', this.angForm.get('area_cultura_2_anos')?.value);
+      formData.append('producao_cultura_2_anos', this.angForm.get('producao_cultura_2_anos')?.value);
+      formData.append('estudo_de_viabilidade', this.angForm.get('estudo_de_viabilidade')?.value);
+      formData.append('termo_compromisso_assinado', this.angForm.get('termo_compromisso_assinado')?.value);
+      formData.append('projeto_riv_completo', this.angForm.get('projeto_riv_completo')?.value);
+      formData.append('ftas', this.angForm.get('ftas')?.value);
+      formData.append('lista_de_trabalhadores', this.angForm.get('lista_de_trabalhadores')?.value);
+      formData.append('documentos_administrativos', this.angForm.get('documentos_administrativos')?.value);
+      formData.append('ficheiro_riv', this.angForm.get('ficheiro_riv')?.value);
+
+      // Tratar o campo 'outros_documentos' para lidar com múltiplos arquivos
+      const outrosDocumentosFiles = this.angForm.get('outros_documentos')?.value;
+      for (let i = 0; i < outrosDocumentosFiles.length; i++) {
+        formData.append('outros_documentos[]', outrosDocumentosFiles[i]);
+      }
+
+      formData.append('data_pn_entregue_ao_pdac', (this.angForm.get('data_pn_entregue_ao_pdac')?.value));
+      formData.append('pn_pendente', this.angForm.get('pn_pendente')?.value);
+      formData.append('justificacao_pn_pendente', this.angForm.get('justificacao_pn_pendente')?.value);
+      formData.append('proponente_desistiu', this.angForm.get('proponente_desistiu')?.value);
+      formData.append('created_at', this.angForm.get('created_at')?.value);
+
+      this.http.post('http://strongboxao.ddns.net:8001/api/v1/formulariosbackoffice/', formData)
+        .subscribe(
+          (response) => {
+            console.log('Formulário enviado com sucesso!', response);
+            // Implemente o código para lidar com a resposta da API aqui
+          },
+          (error) => {
+            console.error('Erro ao enviar o formulário:', error);
+            // Implemente o código para lidar com o erro aqui
+          }
+        );
+    }
+  }
+
+  /*private formatDate(date: string): string {
+    const dateObj = new Date(date);
+    return dateObj.toISOString();
+  }*/
+
+  userbackOff: any;
+  // filtrar usuarios do departamento back off
+  getUserBackOFF() {
+    this.dataService.getUser().subscribe(data => {
+      this.userbackOff = data.filter(user => user.department === 'Back Off');
+      console.log('users do back off: ', data)
+    })
+  }
+
+  checkDates() {
+    const pnEntregueDate = this.angForm.get('inicio_elaboracao_pn')?.value;
+    const fimVerificacaoDate = this.angForm.get('fim_elaboracao_pn')?.value;
+    const today = new Date();
+
+    if (pnEntregueDate > today) {
+      alert('A "Data PN entregue ao PDAC" não pode ser posterior à data de hoje.');
+    }
+
+    if (pnEntregueDate < fimVerificacaoDate) {
+      alert('A "Data PN entregue ao PDAC" não pode ser anterior à "Data fim verificacção PN".');
+    }
+  }
+
+  toggleProponenteDesistiu() {
+    const proponenteDesistiu = this.angForm.get('proponente_desistiu')?.value;
+    this.angForm.get('proponente_desistiu')?.setValue(proponenteDesistiu);
+
+    if (proponenteDesistiu) {
+      alert('O proponente desistiu!');
+    }
+  }
+
+
+
+  toggleJustificacaoPnPendente() {
+    const pnPendente = this.angForm.get('pn_pendente')?.value;
+    if (pnPendente) {
+      this.angForm.get('justificacao_pn_pendente')?.enable();
+    } else {
+      this.angForm.get('justificacao_pn_pendente')?.disable();
+    }
+  }
+
+
 
   inqueritos: any[] = [];
   // lista os inqueritos por ordem do ultimo inquerito gravado e só os inqueritos com estado aprovado
@@ -88,7 +217,7 @@ export class InteressesComponent implements OnInit {
       });
     });
   }
-  
+
 
   downloadFile(url: string): void {
     const link = document.createElement('a');
